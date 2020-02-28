411fe6aab8bdcbf3967c8f199b3083110e357b56
Zain Sirohey <zainsirohey@gmail.com>
2020-02-28 13:11:42 -0500

    Removed unnecessary abstraction for groups and settings

--- DebtLedger/app/src/main/java/cs4474/g9/debtledger/ui/groups/GroupsFragment.java
+++ DebtLedger/app/src/main/java/cs4474/g9/debtledger/ui/groups/GroupsFragment.java
@@ -7,28 +7,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 import cs4474.g9.debtledger.R;
 
 public class GroupsFragment extends Fragment {
 
-    private GroupsViewModel groupsViewModel;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        groupsViewModel =
-                ViewModelProviders.of(this).get(GroupsViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_groups, container, false);
         final TextView textView = root.findViewById(R.id.text_groups);
-        groupsViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        textView.setText("This is groups fragment");
         return root;
     }
 }
