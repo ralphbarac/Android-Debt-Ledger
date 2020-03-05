5b09abb9c024dcf014453a6b5d0f4cfe5757958c
Zain Sirohey <zainsirohey@gmail.com>
2020-03-05 16:15:23 -0500

    Increasing size and style of icons in tabs on dashboard

--- DebtLedger/app/src/main/java/cs4474/g9/debtledger/ui/dashboard/DashboardFragment.java
+++ DebtLedger/app/src/main/java/cs4474/g9/debtledger/ui/dashboard/DashboardFragment.java
@@ -1,10 +1,12 @@
 package cs4474.g9.debtledger.ui.dashboard;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -16,6 +18,9 @@ import cs4474.g9.debtledger.R;
 import cs4474.g9.debtledger.ui.transaction.CreateTransactionActivity;
 
 public class DashboardFragment extends Fragment {
+
+    private final int SEMI_TRANSPARENT = 130;
+    private final int SOLID = 255;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
@@ -39,9 +44,51 @@ public class DashboardFragment extends Fragment {
         viewPager.setAdapter(pageAdapter);
 
         tabLayout.setupWithViewPager(viewPager);
-        tabLayout.getTabAt(0).setIcon(R.drawable.ic_dashboard_owed_black);
-        tabLayout.getTabAt(1).setIcon(R.drawable.ic_dashboard_all_black);
-        tabLayout.getTabAt(2).setIcon(R.drawable.ic_dashboard_owe_black);
+
+        // Creating custom views for tab icons
+        ImageView owedTabView = new ImageView(getContext());
+        owedTabView.setImageResource(R.drawable.ic_dashboard_owed_black);
+        owedTabView.setColorFilter(Color.WHITE);
+        owedTabView.setImageAlpha(SEMI_TRANSPARENT);
+        tabLayout.getTabAt(0).setCustomView(owedTabView);
+
+        ImageView allTabView = new ImageView(getContext());
+        allTabView.setImageResource(R.drawable.ic_dashboard_all_black);
+        allTabView.setColorFilter(Color.WHITE);
+        allTabView.setImageAlpha(SEMI_TRANSPARENT);
+        tabLayout.getTabAt(1).setCustomView(allTabView);
+
+        ImageView oweTabView = new ImageView(getContext());
+        oweTabView.setImageResource(R.drawable.ic_dashboard_owe_black);
+        oweTabView.setColorFilter(Color.WHITE);
+        oweTabView.setImageAlpha(SEMI_TRANSPARENT);
+        tabLayout.getTabAt(2).setCustomView(oweTabView);
+
+        // Listener to change state between solid and semi-transparent of icons on select/un-select
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                View tabView = tab.getCustomView();
+                if (tabView instanceof ImageView) {
+                    ImageView imageView = (ImageView) tabView;
+                    imageView.setImageAlpha(SOLID);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                View tabView = tab.getCustomView();
+                if (tabView instanceof ImageView) {
+                    ImageView imageView = (ImageView) tabView;
+                    imageView.setImageAlpha(SEMI_TRANSPARENT);
+                }
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
         // Initial selected tab should be with "All" Outstanding balances, which appears in the middle, or at position 1
         viewPager.setCurrentItem(1);
