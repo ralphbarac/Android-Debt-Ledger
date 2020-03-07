50680641d1730088171dd0451d9c371cd2c56f23
Thomas Morphew <tmorphew@uwo.ca>
2020-03-07 00:11:21 -0500

    Created UI for contact view and transaction history list items

---
+++ DebtLedger/app/src/main/java/cs4474/g9/debtledger/ViewContactActivity.java
@@ -0,0 +1,14 @@
+package cs4474.g9.debtledger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_contact);
+    }
+}
