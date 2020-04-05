0af473f1fc97c3c82fe211cb77b9bd204d046690
Zain Sirohey <zainsirohey@gmail.com>
2020-04-05 02:01:24 -0400

    Updated colour generator to generate softer tones

--- DebtLedger/app/src/main/java/cs4474/g9/debtledger/logic/ColourGenerator.java
+++ DebtLedger/app/src/main/java/cs4474/g9/debtledger/logic/ColourGenerator.java
@@ -7,22 +7,44 @@ public class ColourGenerator {
     public static int generateFromName(String firstName, String lastName) {
         // A special easter egg
         if (firstName.equals("Mary") && lastName.equals("Sirohey")) {
-            return Color.rgb(184, 26, 181);
+            return Color.rgb(200, 90, 200);
         }
 
-        // TODO: Improve and limit colour generation
         float[] hsv = new float[3];
-        hsv[0] = (firstName.hashCode() + lastName.hashCode()) % 360;
-        hsv[1] = 30;
-        hsv[2] = 30;
+        hsv[0] = Math.abs((firstName + lastName).hashCode()) % 360;
+        // If hue is approximately yellow, adjust, since yellow does not look nice
+        float yellowness = Math.abs(hsv[0] - 60);
+        if (yellowness < 2.5) {
+            hsv[0] = hsv[0] + 60; // Green-ish
+        } else if (yellowness < 5) {
+            hsv[0] = hsv[0] + 240; // Magenta-ish
+        } else if (yellowness < 10) {
+            hsv[0] = hsv[0] + 180; // Blue-ish
+        }
+        hsv[1] = 0.7f;
+        hsv[2] = 1.0f;
         return Color.HSVToColor(hsv);
     }
 
     public static int generateFromGroupName(String groupName) {
+        // A special easter egg
+        if (groupName.toLowerCase().contains("purple")) {
+            return Color.rgb(200, 90, 200);
+        }
+
         float[] hsv = new float[3];
-        hsv[0] = (groupName.hashCode()) % 360;
-        hsv[1] = 30;
-        hsv[2] = 30;
+        hsv[0] = Math.abs(groupName.hashCode()) % 360;
+        // If hue is approximately yellow, adjust, since yellow does not look nice
+        float yellowness = Math.abs(hsv[0] - 60);
+        if (yellowness < 2.5) {
+            hsv[0] = hsv[0] + 60; // Green-ish
+        } else if (yellowness < 5) {
+            hsv[0] = hsv[0] + 240; // Magenta-ish
+        } else if (yellowness < 10) {
+            hsv[0] = hsv[0] + 180; // Blue-ish
+        }
+        hsv[1] = 0.7f;
+        hsv[2] = 1.0f;
         return Color.HSVToColor(hsv);
     }
 }
