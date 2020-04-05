626d60f8edd79794bf72bb72d26380f97643b73e
Owen Hunsburger <ohunsbur@uwo.ca>
2020-04-05 17:33:27 -0400

    Updated alt colours

--- DebtLedger/app/src/main/java/cs4474/g9/debtledger/ui/settings/AccessibleColours.java
+++ DebtLedger/app/src/main/java/cs4474/g9/debtledger/ui/settings/AccessibleColours.java
@@ -6,8 +6,8 @@ public class AccessibleColours {
 
     private static final int positiveColour = Color.GREEN;
     private static final int negativeColor = Color.RED;
-    private static final int positiveAccessibleColour = Color.parseColor("#018571");
-    private static final int negativeAccessibleColour = Color.parseColor("#A6611A");
+    private static final int positiveAccessibleColour = Color.parseColor("#4dac26");
+    private static final int negativeAccessibleColour = Color.parseColor("#d01c8b");
 
     public static int getPositiveColour(boolean isInAccessibleMode) {
         return isInAccessibleMode ? positiveAccessibleColour : positiveColour;
